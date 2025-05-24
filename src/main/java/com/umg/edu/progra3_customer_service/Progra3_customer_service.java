9a425f221c9c4b4e2e5938ed4f7cb633f0035e58
Fernando Sipaque <josefperez84@gmail.com>
2025-05-23 21:59:48 -0600

    Initializes project structure
    Sets up the basic Maven project structure for a customer service application.
    Includes the pom.xml file with project metadata and a simple "Hello World" program.

---
+++ src/main/java/com/umg/edu/progra3_customer_service/Progra3_customer_service.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.umg.edu.progra3_customer_service;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Progra3_customer_service {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
