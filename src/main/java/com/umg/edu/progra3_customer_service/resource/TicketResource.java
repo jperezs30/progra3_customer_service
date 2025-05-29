77af11dd6de83cbe62a2ef6ac467b36103002fa1
Fernando Sipaque <josefperez84@gmail.com>
2025-05-28 23:54:19 -0600

    Renames endpoint and updates dependencies
    Renames the /tickets endpoint to /turno for clarity and consistency.
    
    Updates dependencies by removing the local 'progra3_model' dependency
    and adding 'quarkus-resteasy-reactive-jackson' for improved REST
    support.

--- src/main/java/com/umg/edu/progra3_customer_service/resource/TicketResource.java
+++ src/main/java/com/umg/edu/progra3_customer_service/resource/TicketResource.java
@@ -8,7 +8,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/tickets")
+@Path("/turno")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class TicketResource {
